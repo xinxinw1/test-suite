899de470f1b861415d51dabfa4dfa2092e0f60aa
Xin-Xin Wang <xin-xinw@musiclifephilosophy.com>
2014-10-15 18:49:46 -0400

    add example deps and example tests

---
+++ tests.js
@@ -0,0 +1,18 @@
+/***** Test Suite Testing *****/
+
+//// Sanity ////
+
+test('123', 123);
+test('false', false);
+test('true', true);
+
+function iso(a, b){
+  if (a.length != b.length)return false;
+  for (var i = 0; i < a.length; i++){
+    if (a[i] !== b[i])return false;
+  }
+  return true;
+}
+
+test('[1, 2, 3]', [1, 2, 3], iso);
+
